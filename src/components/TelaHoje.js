35dc8e8a52f249315f3d484056d0f9309c15a2c2
Rubens Algeri <rubens_zys@hotmail.com>
2022-04-01 12:47:47 -0300

    tela hoje concluida

--- src/components/TelaHoje.js
+++ src/components/TelaHoje.js
@@ -9,20 +9,20 @@ import UserContext from "../contexts/UserContext";
 import Topo from "./Topo"
 import Rodape from "./Rodape";
 
-export default function TelaHoje(){
+export default function TelaHoje() {
     const DataHoje = dayjs().locale('pt-br').format('dddd, DD/MM')[0].toUpperCase() + dayjs().locale('pt-br').format('dddd, DD/MM').slice(1);
-    const {state} = useLocation()
+    const { state } = useLocation()
     console.log("habitos", state)
     const setUserData = useContext(UserContext).setUserData
-    const {data, token} = state;
-    const {image} = data;
+    const { data, token } = state;
+    const { image } = data;
     console.log("token ", token)
-    
-    
+
+
     const [listaHabitosHoje, setListaHabitosHoje] = useState([])
     const [habitosCompletos, setHabitosCompletos] = useState(0)
-    
-    const percentage = habitosCompletos/listaHabitosHoje.length*100;
+
+    const percentage = habitosCompletos / listaHabitosHoje.length * 100;
 
 
     useEffect(() => {
@@ -35,65 +35,78 @@ export default function TelaHoje(){
         const promise = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today`, config)
         promise.then((resposta) => {
             setListaHabitosHoje(resposta.data);
-            setHabitosCompletos(resposta.data.filter(habito=>{return habito.done}).length)
+            setHabitosCompletos(resposta.data.filter(habito => { return habito.done }).length)
         })
         promise.catch(() => { alert('Erro, tente novamente mais tarde') })
     }, []);
 
-    function atualizarHabito(status, id){
+    function atualizarHabito(status, id) {
         const config = {
             headers: {
                 Authorization: `Bearer ${token}`
             }
         }
-        if(status){
-            const promessa = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/uncheck`,{}, config)
-            promessa.then(()=>{
-                setHabitosCompletos(habitosCompletos-1)
+        if (status) {
+            const promessa = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/uncheck`, {}, config)
+            promessa.then(() => {
+                setHabitosCompletos(habitosCompletos - 1)
                 console.log("desmarca ", habitosCompletos)
+                const promise = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today`, config)
+                promise.then((resposta) => {
+                    setListaHabitosHoje(resposta.data);
+                    setHabitosCompletos(resposta.data.filter(habito => { return habito.done }).length)
+                })
+                promise.catch(() => { alert('Erro, tente novamente mais tarde') })
+
             })
-            promessa.catch(err=>{
-                console.log("erro ",err.response)
+            promessa.catch(err => {
+                console.log("erro ", err.response)
                 alert(err.response.data.message)
             })
-        }else{
-            const promessas = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/check`,{}, config)
-            promessas.then(()=>{
-                setHabitosCompletos(habitosCompletos+1)
+        } else {
+            const promessas = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${id}/check`, {}, config)
+            promessas.then(() => {
+                setHabitosCompletos(habitosCompletos + 1)
                 console.log("marca ", habitosCompletos)
+                const promise = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today`, config)
+                promise.then((resposta) => {
+                    setListaHabitosHoje(resposta.data);
+                    setHabitosCompletos(resposta.data.filter(habito => { return habito.done }).length)
+                })
+                promise.catch(() => { alert('Erro, tente novamente mais tarde') })
             })
-            promessas.catch(err=>{
+            promessas.catch(err => {
                 console.log("erro ", err.response)
                 alert(err.response.data.message)
             })
         }
     }
 
-    return(
+    return (
         <>
-        <Topo image={image}></Topo>
-        <Habitos completos={percentage}>
-            <div>
-                <p>{DataHoje}</p>
-                {habitosCompletos>0?
-                <em>{percentage}% dos hábitos concluídos</em>
-                :
-                <em>Nenhum hábito concluído ainda</em>}
-            </div>
-            {listaHabitosHoje.length>0? 
-            listaHabitosHoje.map(habito=>{
-                return(
-                    <HabitosCriados sequencia={habito.currentSequence} recorde={habito.highestSequence} feito={habito.done}>
+            <Topo image={image}></Topo>
+            <Habitos completos={percentage}>
+                <div>
+                    <p>{DataHoje}</p>
+                    {habitosCompletos > 0 ?
+                        <em>{percentage}% dos hábitos concluídos</em>
+                        :
+                        <em>Nenhum hábito concluído ainda</em>}
+                </div>
+                {listaHabitosHoje.length > 0 ?
+                    listaHabitosHoje.map(habito => {
+                        return (
+                            <HabitosCriados sequencia={habito.currentSequence} recorde={habito.highestSequence} feito={habito.done}>
 
-                        <h4>{habito.name}</h4>
-                        <h6>Sequência atual: <b className='sequencia'>{habito.currentSequence} dias</b> <br/>Seu recorde: <b className='recorde'>{habito.highestSequence} dias</b></h6>
-                        <ion-icon onClick={()=>atualizarHabito(habito.done, habito.id)} name="checkbox"></ion-icon>
-                    </HabitosCriados>
-                )
-            })
-            :<em>Você não cadastrou nenhum hábito pra hoje. Adicione um hábito para começar a trackear!</em>}
-        </Habitos>
-        <Rodape porcentagem={percentage}></Rodape>
+                                <h4>{habito.name}</h4>
+                                <h6>Sequência atual: <b className='sequencia'>{habito.currentSequence} dias</b> <br />Seu recorde: <b className='recorde'>{habito.highestSequence} dias</b></h6>
+                                <ion-icon onClick={() => atualizarHabito(habito.done, habito.id)} name="checkbox"></ion-icon>
+                            </HabitosCriados>
+                        )
+                    })
+                    : <em>Você não cadastrou nenhum hábito pra hoje. Adicione um hábito para começar a trackear!</em>}
+            </Habitos>
+            <Rodape porcentagem={percentage}></Rodape>
         </>
     )
 }
@@ -132,17 +145,17 @@ h6{
     color: #666666;
 }
 .sequencia{
-    color: ${props=>props.feito?"#8FC549":"#666666"};
+    color: ${props => props.feito ? "#8FC549" : "#666666"};
 }
 .recorde{
-    color: ${props=>props.feito && props.sequencia==props.recorde?"#8FC549":"#666666"};
+    color: ${props => props.feito && props.sequencia == props.recorde ? "#8FC549" : "#666666"};
 }
 ion-icon{
     position: absolute;
     right:13px;
     top: 13px;
     font-size: 69px;
-    color: ${(props)=>props.feito?"#8FC549":'#EBEBEB'};
+    color: ${(props) => props.feito ? "#8FC549" : '#EBEBEB'};
     border: #E7E7E7;
 }
 `
@@ -173,7 +186,7 @@ div em{
     font-size: 17.976px;
     line-height: 22px;
 
-    color: ${(props)=>props.completos>0?"#8FC549":"#BABABA"};
+    color: ${(props) => props.completos > 0 ? "#8FC549" : "#BABABA"};
 }
 em{
     width: calc(33800%/375);
